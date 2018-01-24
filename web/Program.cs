a3a064eda0ddb1dbed1be7333b548a22fd2fb015
denniebee <denniebee@live.com>
2018-01-24 23:45:26 +0100

    Stupid formatting

--- web/Program.cs
+++ web/Program.cs
@@ -8,18 +8,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace tellick_admin
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace tellick_admin {
+    public class Program {
+        public static void Main(string[] args) {
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
     }
 }
--- web/Startup.cs
+++ web/Startup.cs
@@ -9,23 +9,18 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using tellick_admin.Repository;
 
-namespace tellick_admin
-{
-    public class Startup
-    {
+namespace tellick_admin {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<TellickAdminContext>(opt => opt.UseInMemoryDatabase("TellickAdmin"));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
