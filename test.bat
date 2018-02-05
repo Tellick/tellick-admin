4cb1e11e3894319510307db9c564ce25a8f79221
denniebee <denniebee@live.com>
2018-02-05 13:47:02 +0100

    Updated test

--- test.bat
+++ test.bat
@@ -4,3 +4,6 @@ dotnet cli\bin\Debug\netcoreapp2.0\cli.dll new project tellickadmin tellick
 dotnet cli\bin\Debug\netcoreapp2.0\cli.dll active tellickadmin
 dotnet cli\bin\Debug\netcoreapp2.0\cli.dll log 4 "First"
 dotnet cli\bin\Debug\netcoreapp2.0\cli.dll log 4 "Second" 2018-2-1
+dotnet cli\bin\Debug\netcoreapp2.0\cli.dll show
+dotnet cli\bin\Debug\netcoreapp2.0\cli.dll show 2018-2
+dotnet cli\bin\Debug\netcoreapp2.0\cli.dll show 2018
